9d1a590f01df480884f849119b52b5a91ebffdd8
Aman Sharma <amans@geekyants.com>
2022-02-07 23:47:52 +0530

    added constructor to initialize CDLP and save CDT address

---
+++ contracts/Exchange.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Exchange is ERC20 {
+    address public cryptoDevTokenAddress;
+
+    constructor(address _CryptoDevToken) ERC20("CryptoDev LP Token", "CDLP") {
+        require(
+            _CryptoDevToken != address(0),
+            "Token address passed is a null address"
+        );
+        cryptoDevTokenAddress = _CryptoDevToken;
+    }
+}
