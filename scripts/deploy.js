928e3f407f81a75cb91438acaf635d0ea0e1449d
Aman Sharma <amans@geekyants.com>
2022-02-08 22:54:12 +0530

    added deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS } = require("../constants");
+
+async function main() {
+  const cryptoDevTokenAddress = CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS;
+  const exchangeContract = await ethers.getContractFactory("Exchange");
+  const deployedExchangeContract = await exchangeContract.deploy(
+    cryptoDevTokenAddress,
+  );
+  console.log("Exchange Contract Address: ", deployedExchangeContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
